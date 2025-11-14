641967006fa5487991a78f60f67c77e566fff316
+ <+>
2025-11-14 16:13:34 +1100

    Event Handling in React Lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+const EyesOnMe = () => {
+    return (
+        <div>
+            <button
+                onFocus={() => console.log("Good!")}
+                onBlur={() => console.log("Hey! Eyes on me!")}
+                on >Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
